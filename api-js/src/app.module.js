0fa0b523def6c82f9d247d624d0d3e5562df7829
Daniel Santos <dsantosgames20@gmail.com>
2025-04-10 16:09:49 -0300

    configurando projeto para deploy no Render

--- api-js/src/app.module.js
+++ api-js/src/app.module.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://fireforgelabs.vercel.app/", credentials: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use(AuthModule, UserModule, ProductsModule, CartModule, OrdersModule);
