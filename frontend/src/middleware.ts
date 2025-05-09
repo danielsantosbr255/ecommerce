3767ab12c667ebadc0d4a7af30eac024c1f62896
Daniel Santos <danielsantosbr255@gmail.com>
2025-05-09 17:19:32 -0300

    try_fix: removido domain do cookie. Desnecessário

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,7 +6,7 @@ const protectedRoutes = ["/account", "/admin"];
 
 export function middleware(request: NextRequest) {
   const refreshToken = request.cookies.get("refreshToken")?.value;
-  console.log(refreshToken)
+  console.log("refreshToken", refreshToken)
 
   if (protectedRoutes.includes(request.nextUrl.pathname)) {
     if (!refreshToken) {
