2fd3938ca14afc831ab40aa43c1bc468dc93edb7
Daniel Santos <danielsantosbr255@gmail.com>
2025-06-12 20:14:08 -0300

    refactor: removido projeto teste usando nestjs

--- api-nestjs/src/admin/admin.service.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Injectable()
-export class AdminService {}
--- api-nestjs/src/users/user.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: string; // ObjectId do MongoDB é armazenado como string
-  name: string;
-  email: string;
-  password: string;
-  role: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
